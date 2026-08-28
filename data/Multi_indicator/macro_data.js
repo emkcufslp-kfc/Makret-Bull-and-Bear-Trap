bfc1103660969ff5026f7ce68776c380391ad68a
Fung <emkcufslp@gmail.com>
2026-08-29 02:27:03 +0800

    Update dashboards and strategies

--- data/Multi_indicator/macro_data.js
+++ data/Multi_indicator/macro_data.js
@@ -1,8 +1,8 @@
 const macroData = {
-    "last_updated": "2026-08-27 06:16:16",
-    "indicator_1": 1.98,
-    "indicator_2": 8.44,
-    "indicator_3": -1.52,
-    "vix": 15.21,
+    "last_updated": "2026-08-28 06:17:07",
+    "indicator_1": 1.57,
+    "indicator_2": 9.07,
+    "indicator_3": -0.87,
+    "vix": 14.51,
     "status": "Green"
 };
