0e866e558b93d4136a0d2783e3b6bdd3ec418dcb
Fung <emkcufslp@gmail.com>
2026-09-21 06:44:25 +0800

    chore: daily refresh 2026-09-21

--- data/Multi_indicator/macro_data.js
+++ data/Multi_indicator/macro_data.js
@@ -1,8 +1,8 @@
 const macroData = {
-    "last_updated": "2026-09-19 06:15:53",
-    "indicator_1": 2.24,
-    "indicator_2": 6.64,
-    "indicator_3": -2.08,
+    "last_updated": "2026-09-21 06:17:26",
+    "indicator_1": 1.99,
+    "indicator_2": 6.91,
+    "indicator_3": -1.84,
     "vix": 14.81,
     "status": "Green"
 };
